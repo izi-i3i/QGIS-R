b3a97ae02d5afc46153294e352d3a651bccad3f4
Izi <izi31416@protonmail.com>
2025-05-21 12:03:47 -0300

    Update plot_variogram.R

--- rscrits/functions/plot_variogram.R
+++ rscrits/functions/plot_variogram.R
@@ -9,26 +9,26 @@ plot_variogram = function(vg, fit.vgm, model = NULL) {
   preds = variogramLine(fit.vgm, maxdist = max(vg$dist))
   breaks = pretty(range(vg$np), n = nclass.Sturges(vg$np)-1, min.n = 1)
 
-  MDF = as.data.frame(fit.vgm)[c("psill", "range")]
+  VAR_DF = as.data.frame(fit.vgm)[c("psill", "range")]
 
   if(length(fit.vgm$model) < 2)
   {
     nrp_line = list(
-      geom_hline(yintercept = max(MDF$psill), linetype = 2, color = "gray55"),
-      geom_vline(xintercept = max(MDF$range), linetype = 2, color = "gray55"),
-       geom_segment(data=MDF, aes(x = 0, y = 0, xend = range, yend = 0, color="Range"),
+      geom_hline(yintercept = max(VAR_DF$psill), linetype = 2, color = "gray55"),
+      geom_vline(xintercept = max(VAR_DF$range), linetype = 2, color = "gray55"),
+       geom_segment(data=VAR_DF, aes(x = 0, y = 0, xend = range, yend = 0, color="Range"),
                     linetype = 1, arrow = arrow(length = unit(0.15,"cm"), ends="both")),
-       geom_segment(data=MDF, aes(x = max(range), y=0, xend=max(range), yend=max(psill), color="Psill"),
+       geom_segment(data=VAR_DF, aes(x = max(range), y=0, xend=max(range), yend=max(psill), color="Psill"),
                     linetype = 1, arrow = arrow(length = unit(0.15,"cm"), ends="both"))
     )
   } else {
-    L1 = data.frame(x = 0, y = 0, xend = 0, yend = min(MDF$psill))
-    L2 = data.frame(x = 0, y = min(MDF$psill), xend = max(MDF$range), yend = min(MDF$psill))
-    L3 = data.frame(x = max(MDF$range), y = min(MDF$psill), xend = max(MDF$range), yend = max(MDF$psill))
+    L1 = data.frame(x = 0, y = 0, xend = 0, yend = min(VAR_DF$psill))
+    L2 = data.frame(x = 0, y = min(VAR_DF$psill), xend = max(VAR_DF$range), yend = min(VAR_DF$psill))
+    L3 = data.frame(x = max(VAR_DF$range), y = min(VAR_DF$psill), xend = max(VAR_DF$range), yend = max(VAR_DF$psill))
 
     nrp_line = list(
-      geom_hline(yintercept=max(MDF$psill), linetype=2, color="gray55"),
-      geom_vline(xintercept=max(MDF$range), linetype=2, color="gray55"),
+      geom_hline(yintercept=max(VAR_DF$psill), linetype=2, color="gray55"),
+      geom_vline(xintercept=max(VAR_DF$range), linetype=2, color="gray55"),
        geom_segment(data=L1, aes(x = x, y = y, xend = xend, yend = yend, color="Nugget"),
                     linetype = 1, arrow = arrow(length = unit(0.15,"cm"), ends="both")),
        geom_segment(data=L2, aes(x = x, y = y, xend = xend, yend = yend, color="Range"),
